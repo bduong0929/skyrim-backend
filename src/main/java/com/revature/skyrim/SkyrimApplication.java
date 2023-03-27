3e4fb4d1eb98200bb65df35cde4c94ee4d24ef7c
Bao Duong <baoduongxy@gmail.com>
2023-03-27 19:17:23 -0400

    template done

---
+++ src/main/java/com/revature/skyrim/SkyrimApplication.java
@@ -0,0 +1,11 @@
+package com.revature.skyrim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyrimApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SkyrimApplication.class, args);
+	}
+}
